95329d0c62f992326aa10272ad710e3d295c1a6d
Mahmood <mahmoodateya75@gmail.com>
2025-12-04 09:16:50 +0300

    Main Firebase config

---
+++ Food2Share_Main/Food2Share_Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food2Share_Main
+//
+//  Created by BP-36-201-10 on 04/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
