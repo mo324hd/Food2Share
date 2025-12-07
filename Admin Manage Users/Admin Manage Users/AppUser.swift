40c0c9c9e92be70d0a3bd0b17e500496f048d3d3
Mahmood <mahmoodateya75@gmail.com>
2025-12-07 09:22:04 +0300

    Basic Users table - make functional buttons next

---
+++ Admin Manage Users/Admin Manage Users/AppUser.swift
@@ -0,0 +1,8 @@
+//
+//  AppUser.swift
+//  Admin Manage Users
+//
+//  Created by BP-36-201-10 on 07/12/2025.
+//
+
+import Foundation
---
+++ Admin Manage Users/Admin Manage Users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Admin Manage Users
+//
+//  Created by BP-36-201-10 on 07/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
