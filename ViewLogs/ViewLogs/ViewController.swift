fb9c93336920b69f1c4118847cc24fe790b07738
mahmood <mahmoodateya75@gmail.com>
2025-12-23 12:14:08 +0300

    Start of view logs

---
+++ ViewLogs/ViewLogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewLogs
+//
+//  Created by BP-19-114-19 on 23/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
