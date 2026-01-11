61fabae25be9fb03f2b5afa455d1cd01238938a6
NM <n/a>
2026-01-11 07:44:52 +0300

    connected my work with the work of the group

---
+++ F2S_MAIN/F2SAuthicate/Controllers/AddFoodItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddFoodItemViewController.swift
+//  Food2Share_Main
+//
+//  Created by N M on 1/11/26.
+//
+
+import UIKit
+
+class AddFoodItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ F2S_MAIN/F2SAuthicate/Nibs/SampleDataInAFile.swift
@@ -0,0 +1,39 @@
+//
+//  SampleDataInAFile.swift
+//  Food2Share_Main
+//
+//  Created by N M on 1/7/26.
+//
+
+import Foundation
+
+struct Campaign {
+    var id: String
+    var name: String
+    var progressNumber: Int
+    var goalNumber: Int
+    var organizedBy: String
+    var dateValue: String
+    var description: String
+}
+
+struct Organization {
+    var id: String
+    var name: String
+    var campaigns: [Campaign]
+    var description: String
+    var address: String
+    var communication: String
+}
+
+struct FoodItem {
+    var id: String
+    var name: String
+    var category: String
+    var quantity_Size: String
+    var dateValue: String
+    var status: String
+    var usage_Condition: String
+}
+
+
